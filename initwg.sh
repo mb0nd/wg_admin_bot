34a32de1648cfe11edbef4902976f0690d62bc0a
root <root@v1037627.hosted-by-vdsina.ru>
2022-10-18 21:39:45 +0300

    Установил права для конфига

--- initwg.sh
+++ initwg.sh
@@ -4,6 +4,7 @@ if [ ! -f /etc/wireguard/wg0.conf ]; then
     wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
     chmod 600 /etc/wireguard/privatekey
     echo -e "[Interface]\nPrivateKey = $(cat /etc/wireguard/privatekey)\nAddress = 10.0.0.1/24\nListenPort = "$LISTEN_PORT"\nPostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\nPostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" > /etc/wireguard/wg0.conf
+    chmod 600 /etc/wireguard/wg0.conf
 fi
 wg-quick up wg0
 python3 bot/__main__.py
