885f70865f036dc7895ef0a0f1ee0cb8f7afa923
mbondarenko <mbondarenko@samsonpost.ru>
2023-01-13 11:54:12 +0000

    Рефакторинг wg_services

--- bot/modules/wg_services.py
+++ bot/modules/wg_services.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union
+from typing import Dict, List
 from db.models import User
 import subprocess
 import datetime
@@ -16,42 +16,36 @@ def check_username(name: str) -> bool:
         return True
     return False
 
-async def check_statistics() -> Dict:
-    """Выполняет команду "wg show" и парсит вывод о каждом пользователе в словарь
-
-    Returns:
-        List[Dict]: данные об использовании страфика 
-    """
-    output = (x.strip().split(': ') for x in subprocess.getoutput('wg show').splitlines()[5:] if x)
-    result = {}
-    for k, v in output:
-        if k == 'peer':
-            key = v
-            result[key] = {}
-        else:
-            result[key][k] = v
-    return result
-    
-async def data_preparation(data_db: Union[list, User]) -> str:
+async def data_preparation(data_db: List[User]) -> str:
     """Собирает вместе данные из БД и консоли Wireguard и перегоняет в удобный вид
 
     Args:
-        data_db (Union[list, User]): список объектов пользователей из БД
+        data_db (List[User]): список объектов пользователей из БД
 
     Returns:
         str: подготовленная для вывода строка со статистикой пользователей
     """
-    data_cmd = await check_statistics()
-    res=''
+    result = '' 
+    user_statistics_list = []
+    data_cmd = await _check_statistics()
     for user in data_db:
+        user_statistic = {
+                'Имя': user.user_name,
+                'Локальный адрес': user.ip,
+                'Статус': 'заблокирован' if user.is_baned else 'активен',
+                'Внешний адрес/порт': 'нет данных',
+                'Появлялся:': 'нет данных',
+                'Трафик': 'нет данных'}
         peer: dict = data_cmd.get(user.pub_key)
         if peer and peer.get('endpoint'):
-            res += f"<b>Имя:</b> <code>{user.user_name}</code>\n<b>Локальный адрес:</b> <code>{user.ip}</code>\n<b>Активен:</b> <code>{'нет' if user.is_baned else 'да'}</code>\n<b>Внешний адрес/порт</b>: <code>{peer['endpoint']}</code>\n<b>Появлялся:</b> <code>{_prepare_time_data(peer['latest handshake'])}</code>\n<b>Трафик:</b> <code>{peer['transfer']}</code>\n{'_'*50}\n"
-        else:
-            res += f"<b>Имя:</b> <code>{user.user_name}</code>\n<b>Локальный адрес:</b> <code>{user.ip}</code>\n<b>Активен:</b> <code>{'нет' if user.is_baned else 'да'}</code>\n{'_'*50}\n"
-    if not res:
-        return 'Не возможно отобразить статистику, пользователь(и) не найден в wireguard'
-    return res
+            user_statistic['Внешний адрес/порт'] = peer['endpoint']
+            user_statistic['Появлялся:'] = _prepare_time_data(peer['latest handshake'])
+            user_statistic['Трафик'] = peer['transfer']
+        user_statistics_list.append(user_statistic)
+    # Тут можно делать сортировки
+    for user in user_statistics_list:
+        result +="\n".join(map(lambda x: f"<b>{x[0]}:</b> <code>{x[1]}</code>", user.items())) + f"\n{'_'*32}\n"
+    return result
 
 async def restart_wg() -> tuple:
     """Выполняет команду перезапуска сервера Wireguard
@@ -69,6 +63,22 @@ async def restart_wg() -> tuple:
     except subprocess.CalledProcessError:
         return (f"Что то пошло не так", False)
 
+async def _check_statistics() -> Dict:
+    """Выполняет команду "wg show" и парсит вывод о каждом пользователе в словарь
+
+    Returns:
+        List[Dict]: данные об использовании страфика 
+    """
+    output = (x.strip().split(': ') for x in subprocess.getoutput('wg show').splitlines()[5:] if x)
+    result = {}
+    for k, v in output:
+        if k == 'peer':
+            key = v
+            result[key] = {}
+        else:
+            result[key][k] = v
+    return result
+
 def _prepare_time_data(time_string: str) -> str:
     """Подготавливает строку с датой и временем к нужному виду,
     которая показывает когда клиент подключался последний раз
@@ -76,11 +86,11 @@ def _prepare_time_data(time_string: str) -> str:
     Returns:
         str: строка формата '19:12:32 12.01.2023'
     """
-    def _get_timedelta(seconds: int = 0, minutes: int = 0, hours: int = 0) -> datetime:
+    def _get_timedelta(seconds: int = 0, minutes: int = 0, hours: int = 0, days: int = 0) -> datetime:
         """Вычисляет разницу во времени вычитая прошедшее с последнего подключения
         из текущего
         """
-        delta = datetime.timedelta(seconds=seconds, minutes=minutes, hours=hours)
+        delta = datetime.timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days)
         return datetime.datetime.now() - delta
     prepare_data = list(map(lambda x: int(x.split()[0]), time_string.split(', ')))
     prepare_data.reverse()
