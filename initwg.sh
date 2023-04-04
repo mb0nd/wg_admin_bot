5a4079cbb65ad0ec5c9ef3091fec855d75e561a5
mbondarenko <mbondarenko@samsonpost.ru>
2023-04-04 16:29:24 +0000

    Fix interface name with create wg0.conf

--- initwg.sh
+++ initwg.sh
@@ -3,7 +3,7 @@
 if [ ! -f "$PATH_TO_WG"wg0.conf ]; then
     wg genkey | tee "$PATH_TO_WG"privatekey | wg pubkey | tee "$PATH_TO_WG"publickey
     chmod 600 "$PATH_TO_WG"privatekey
-    echo -e "[Interface]\nPrivateKey = $(cat "$PATH_TO_WG"privatekey)\nAddress = 10.0.0.1/24\nListenPort = "$LISTEN_PORT"\nPostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\nPostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" > "$PATH_TO_WG"wg0.conf
+    echo -e "[Interface]\nPrivateKey = $(cat "$PATH_TO_WG"privatekey)\nAddress = 10.0.0.1/24\nListenPort = "$LISTEN_PORT"\nPostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o `ip route | awk '/default/ {print $5; exit}'` -j MASQUERADE\nPostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o `ip route | awk '/default/ {print $5; exit}'` -j MASQUERADE" > "$PATH_TO_WG"wg0.conf
     chmod 600 "$PATH_TO_WG"wg0.conf
 fi
 wg-quick up wg0
